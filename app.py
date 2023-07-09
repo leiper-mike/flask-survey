b3944c1cd1c126eb6a1356030a9cd2e0c6a6a8eb
leiper-mike <74512712+leiper-mike@users.noreply.github.com>
2023-07-09 19:40:00 -0400

    implemented survey questions, url safety

--- app.py
+++ app.py
@@ -1,3 +1,35 @@
-from flask import Flask
-
+from flask import Flask, request, render_template, redirect, flash
+from surveys import satisfaction_survey
 app = Flask(__name__)
+app.secret_key = 'abc123'
+responses = []
+finished = []
+@app.route('/')
+def base():
+    return render_template("root.html", surveyTitle = satisfaction_survey.title, surveyInstructions = satisfaction_survey.instructions)
+
+@app.route('/questions/<int:num>')
+def question(num):
+    if(num == len(satisfaction_survey.questions)):
+        if(len(finished) > 0):
+            flash("You have already completed the survey")
+            return redirect("/thanks")
+        finished.append(0)
+        return redirect("/thanks")
+    if(not (num == len(responses)) or num >= len(satisfaction_survey.questions)):
+        flash("Invalid question ID")
+        return redirect(f"/questions/{len(responses)}")
+    question = satisfaction_survey.questions[num]
+    return render_template("question.html", questionText = question.question, questionChoices = question.choices, allow_text = question.allow_text, surveyTitle = satisfaction_survey.title)
+
+@app.route('/answer', methods=["POST"])
+def answer():
+    responses.append(request.form["answer"])
+    return redirect(f"/questions/{len(responses)}")
+
+@app.route('/thanks')
+def thanks():
+    if(not len(responses) == len(satisfaction_survey.questions)):
+        flash("You must complete the survey before we thank you.")
+        return redirect(f"questions/{len(responses)}")
+    return render_template("thanks.html", surveyName = satisfaction_survey.title)
