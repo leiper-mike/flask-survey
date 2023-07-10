45c6d67a9b07f0a68967c6693654eab945378326
leiper-mike <74512712+leiper-mike@users.noreply.github.com>
2023-07-10 14:07:21 -0400

    implemented session

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from surveys import satisfaction_survey
 app = Flask(__name__)
 app.secret_key = 'abc123'
-responses = []
-finished = []
 @app.route('/')
 def base():
     return render_template("root.html", surveyTitle = satisfaction_survey.title, surveyInstructions = satisfaction_survey.instructions)
 
 @app.route('/questions/<int:num>')
 def question(num):
+    responses = session["answers"]
     if(num == len(satisfaction_survey.questions)):
-        if(len(finished) > 0):
+        if(session["finished"]):
             flash("You have already completed the survey")
             return redirect("/thanks")
-        finished.append(0)
+        session["finished"] = True
         return redirect("/thanks")
     if(not (num == len(responses)) or num >= len(satisfaction_survey.questions)):
         flash("Invalid question ID")
@@ -24,12 +23,21 @@ def question(num):
 
 @app.route('/answer', methods=["POST"])
 def answer():
-    responses.append(request.form["answer"])
-    return redirect(f"/questions/{len(responses)}")
+    answers = session["answers"]
+    answers.append(request.form["answer"])
+    session["answers"] = answers
+    return redirect(f"/questions/{len(answers)}")
 
 @app.route('/thanks')
 def thanks():
+    responses = session["answers"]
     if(not len(responses) == len(satisfaction_survey.questions)):
         flash("You must complete the survey before we thank you.")
         return redirect(f"questions/{len(responses)}")
     return render_template("thanks.html", surveyName = satisfaction_survey.title)
+
+@app.route("/start", methods=["POST"])
+def start():
+    session["answers"] = []
+    session["finished"] = False
+    return redirect("/questions/0")
